9d6bbafe4e8b0afa29f84315f306ccb6efced050
John Mistica <79333427+johnmistica0@users.noreply.github.com>
2023-11-10 08:22:47 -0600

    Update page.tsx

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,44 +1,29 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { FishSymbol, X } from "lucide-react";
-import { useEffect, useState } from "react";
+import { X } from "lucide-react";
+import { useState } from "react";
 import { useTheme } from "next-themes";
 import {
   APIProvider,
   Map,
   AdvancedMarker,
-  InfoWindow,
   useAdvancedMarkerRef,
 } from "@vis.gl/react-google-maps"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card } from "@/components/ui/card";
-import CardWithForm from "@/components/CardWithForm";
 import CatchInfoCard from "@/components/CatchInfoCard";
 
-
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const {theme} = useTheme()
   const position = { lat: 30.393951, lng: -97.725604 }
-  const { theme } = useTheme()
-  const options = {
+  const mapOptions = {
     zoom: 14,
     controlSize: 30,
     fullscreenControl: false,
     streetViewControl: false,
-    mapId: theme === 'dark' ? process.env.NEXT_PUBLIC_DARK_MAP_ID : process.env.NEXT_PUBLIC_LIGHT_MAP_ID,
+    mapId: theme === 'dark' ? process.env.NEXT_PUBLIC_DARK_MAP_ID : process.env.NEXT_PUBLIC_LIGHT_MAP_ID
   }
-  const [mapOptions, setMapOptions] = useState(options)
-  const [mapKey, setMapKey] = useState(1);
-  const [open, setOpen] = useState(false);
-  const [markerRef, marker] = useAdvancedMarkerRef();
-
-  useEffect(() => {
-    setMapOptions((prevState) => ({
-      ...prevState,
-      mapId: theme === 'dark' ? process.env.NEXT_PUBLIC_DARK_MAP_ID : process.env.NEXT_PUBLIC_LIGHT_MAP_ID
-    }));
-    setMapKey((prevKey) => prevKey + 1);
-  }, [theme]);
 
   return (
     <main className="grid grid-cols-4 h-[calc(100vh-64px)]">
@@ -52,7 +37,7 @@ export default function Home() {
       </div>
       <div className="col-span-3">
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ''}>
-          <Map key={mapKey} center={position} {...mapOptions}>
+          <Map center={position} {...mapOptions}>
             <AdvancedMarker ref={markerRef} position={position}>
               {open ? 
               <CatchInfoCard closeClickHandler={() => {setOpen(false)}}/> : 
