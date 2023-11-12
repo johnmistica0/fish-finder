8e7167441894a1b5b7bd3ba32020e4b1eec0ff16
John Mistica <79333427+johnmistica0@users.noreply.github.com>
2023-11-12 17:24:57 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  router.push('/explore')
+
+  useEffect(() => {
+    router.push('/explore')
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <main className="flex flex-col items-center justify-between p-24 h-max space-y-10">
-      Home
+      Social
     </main>
   )
 }
